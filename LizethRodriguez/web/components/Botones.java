5942a2ff50a1a1e40ab938ce959b2ebf84cb305d
BitsLizethRodriguez <55768230+BitsLizethRodriguez@users.noreply.github.com>
2019-10-02 11:02:24 -0500

    Botones

---
+++ LizethRodriguez/web/components/Botones.java
@@ -0,0 +1,63 @@
+package web.components;
+
+public class Botones {
+
+    private String name;
+    private String color;
+    private String logo;
+    private String url;
+    private boolean new_window;
+
+    //Declarate a constructor
+    public  Botones(String name1, String url1){
+        name = name1;
+        url = url1;
+        color = "blue";
+        logo = "http://www.nashikproperty.com/uploads/builder-logo/default-logo.png";
+        new_window = true;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setNew_window(boolean new_window) {
+        this.new_window = new_window;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public boolean isNew_window() {
+        return new_window;
+    }
+
+}
+
+
+
+
