d0ce9e39f5f421ff6d52cd4f0c6b7a08e089e371
BitsJacquelineBuitrago <jbuitrag77@gmail.com>
2019-10-25 11:50:51 -0500

    Update TestPageBits.java
    Ejercicio Pagina Bits

--- JacquelineBuitrago/paginaBits/TestPageBits.java
+++ JacquelineBuitrago/paginaBits/TestPageBits.java
@@ -13,7 +13,7 @@ public class TestPageBits {
 
     @Before
     public void abrirDriver() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/chromedriver.exe");
 
        driver = new ChromeDriver();
 
@@ -27,16 +27,16 @@ public class TestPageBits {
         driver.get("https://www.bitsamericas.com");
 
         driver.findElement(By.xpath("//a[contains(text(),'Contáctenos')]")).click();
-        driver.findElement(By.xpath("//input[@id='contact_us__correo_electronico']")).sendKeys("orfenys.perez@bitsamericas.com");
-        driver.findElement(By.xpath("//input[@id='contact_us__telefono']")).sendKeys("1234");
-        driver.findElement(By.xpath("//input[@id='contact_us__nombre']")).sendKeys("Orfenys Perez");
+        driver.findElement(By.xpath("//input[@id='contact_us__correo_electronico']")).sendKeys("Jacqueline.Buitrago@bitsamericas.com");
+        driver.findElement(By.xpath("//input[@id='contact_us__telefono']")).sendKeys("2345678");
+        driver.findElement(By.xpath("//input[@id='contact_us__nombre']")).sendKeys("Jacqueline Buitrago");
         driver.findElement(By.xpath("//input[@id='contact_us__organizacion']")).sendKeys("Bits");
         driver.findElement(By.xpath("//ng-select[@id='contact_us__pais']//span[@class='btn btn-default btn-secondary form-control ui-select-toggle']")).sendKeys("Colombia");
         driver.findElement(By.xpath("//input[@id='contact_us__ciudad']")).sendKeys("Bogotá");
         driver.findElement(By.xpath("//ng-select[@id='contact_us__servicios_de_interes']//span[@class='btn btn-default btn-secondary form-control ui-select-toggle']")).sendKeys("Desarrollo");
         driver.findElement(By.xpath("//div[@class='ui-select-choices-row active']//a[@class='dropdown-item']")).click();
         driver.findElement(By.xpath("//ng-select[@id='contact_us__productos_de_interes']//span[@class='btn btn-default btn-secondary form-control ui-select-toggle']")).sendKeys("iMedical");
-        driver.findElement(By.xpath("//textarea[@id='contact_us__comentario']")).sendKeys("Un comentario");
+        driver.findElement(By.xpath("//textarea[@id='contact_us__comentario']")).sendKeys("Prueba Bits");
         driver.findElement(By.xpath("//form[@id='formulario_contacto']//label[contains(text(),'He leído y acepto los')]")).click();
 
         Thread.sleep(4000);
